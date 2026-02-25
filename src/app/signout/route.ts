9a23c6b8bc58bf01699435cdec3f59a23a35c655
mmiller1444 <35677659+mmiller1444@users.noreply.github.com>
2026-02-25 16:26:22 -0700

    Initial commit

--- src/app/signout/route.ts
+++ src/app/signout/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { createClient } from "../../lib/supabase/server";
 
-export async function GET() {
+export async function GET(request: Request) {
   const supabase = createClient();
   await supabase.auth.signOut();
-  return NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"));
+  return NextResponse.redirect(new URL("/login", request.url));
 }
